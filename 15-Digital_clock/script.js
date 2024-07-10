b3140b0114a6fec786d627716cc1dafda9b66458
Bhargavi Amillineni <147232054+bunnybhargavi@users.noreply.github.com>
2024-07-10 11:52:57 +0530

    Add files via upload

---
+++ 15-Digital_clock/script.js
@@ -0,0 +1,29 @@
+
+//call the function when page reload
+window.addEventListener('load',calculateTime);
+
+function calculateTime(){
+  var date = new Date();
+  var dayNumber = date.getDay();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var ampm = hour > 12? 'PM' :'AM';
+  var dayNames = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+
+  // to get time in 12 hours 
+
+  hour = hour % 12;
+  hour = hour==0 ? '12':hour;
+  hour = hour<10 ? '0'+hour:hour;
+  min = min<10 ? '0'+min:min;
+
+  document.getElementById("day").innerHTML = dayNames[dayNumber];
+  document.getElementById("hour").innerHTML = hour
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("ampm").innerHTML = ampm;
+
+  //to display live time
+
+  setTimeout(calculateTime,100);
+
+}
